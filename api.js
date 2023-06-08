ef8750f5a06bf2797c71581ba99dd1fe0d3dd439
denniztr <denniztr@gmail.com>
2023-06-08 12:10:20 +0300

    first commit

---
+++ api.js
@@ -0,0 +1,73 @@
+const host = "https://webdev-hw-api.vercel.app/api/v2/todos";
+
+export function getTodos({ token }) {
+  return fetch(host, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  }).then((response) => {
+    if (response.status === 401) {
+      throw new Error("Нет авторизации");
+    }
+
+    return response.json();
+  });
+}
+
+export function deleteTodo({ token, id }) {
+  return fetch("https://webdev-hw-api.vercel.app/api/todos/" + id, {
+    method: "DELETE",
+    headers: {
+      Authorization: token,
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+export function addTodo({ text, token }) {
+  return fetch(host, {
+    method: "POST",
+    body: JSON.stringify({
+      text,
+    }),
+    headers: {
+      Authorization: token,
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+// https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
+export function registerUser({ login, password, name }) {
+  return fetch("https://webdev-hw-api.vercel.app/api/user", {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      password,
+      name,
+    }),
+  }).then((response) => {
+    if (response.status === 400) {
+      throw new Error("Такой пользователь уже существует");
+    }
+    return response.json();
+  });
+}
+
+export function loginUser({ login, password }) {
+  return fetch("https://webdev-hw-api.vercel.app/api/user/login", {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      password,
+    }),
+  }).then((response) => {
+    if (response.status === 400) {
+      throw new Error("Неверный логин или пароль");
+    }
+    return response.json();
+  });
+}
---
+++ index.js
@@ -0,0 +1,143 @@
+// План
+// 1. Реализовать форму логина в приложении (+)
+//  * Перенести всю разметку в рендер функцию (+)
+//  * Сделать форму входа динамическй (+)
+//  * отрефакторить приложение на модули
+//    * api (+)
+//    * вытищть логин компонент в отдельный модуль (+)
+//    * TODO: вытащить компонент списка задач и форму добавления в отдельный модуль
+// 2. Реализовать форму регистрации (+)
+
+import { addTodo, deleteTodo, getTodos } from "./api.js";
+import { renderLoginComponent } from "./components/login-component.js";
+import { formatDateToRu, formatDateToUs } from "./lib/formatDate/formatDate.js";
+import { format } from "date-fns";
+
+let tasks = [];
+
+let token = "Bearer asb4c4boc86gasb4c4boc86g37k3bk3cg3c03ck3k37w3cc3bo3b8";
+
+const country = 'ru';
+
+token = null;
+
+const fetchTodosAndRender = () => {
+  return getTodos({ token }).then((responseData) => {
+    tasks = responseData.todos;
+    renderApp();
+  });
+};
+
+const renderApp = () => {
+  const appEl = document.getElementById("app");
+  if (!token) {
+    renderLoginComponent({
+      appEl,
+      setToken: (newToken) => {
+        token = newToken;
+      },
+      fetchTodosAndRender,
+    });
+
+    return;
+  }
+
+
+  const tasksHtml = tasks
+    .map((task) => {
+      const createDate = format(new Date(task.created_at), 'dd.MM.yyyy hh:mm:ss');
+      return `
+          <li class="task">
+            <p class="task-text">
+              ${task.text} (Создал: ${task.user?.name ?? "Неизвестно"})
+              <button data-id="${
+                task.id
+              }" class="button delete-button">Удалить</button>
+            </p>
+            <p> <i> Задача создана: ${createDate} </i> </p>
+          </li>`;
+    })
+    .join("");
+
+  const appHtml = `
+                <h1>Список задач</h1>
+
+                <ul class="tasks" id="list">
+                <!-- Список рендерится из JS -->
+                ${tasksHtml}
+                </ul>
+                <br />
+                <div class="form">
+                <h3 class="form-title">Форма добавления</h3>
+                <div class="form-row">
+                    Что нужно сделать:
+                    <input
+                    type="text"
+                    id="text-input"
+                    class="input"
+                    placeholder="Выпить кофе"
+                    />
+                </div>
+                <br />
+                <button class="button" id="add-button">Добавить</button>
+                </div>`;
+
+  appEl.innerHTML = appHtml;
+
+  const buttonElement = document.getElementById("add-button");
+  const listElement = document.getElementById("list");
+  const textInputElement = document.getElementById("text-input");
+
+  const deleteButtons = document.querySelectorAll(".delete-button");
+
+  for (const deleteButton of deleteButtons) {
+    deleteButton.addEventListener("click", (event) => {
+      event.stopPropagation();
+
+      const id = deleteButton.dataset.id;
+
+      // подписываемся на успешное завершение запроса с помощью then
+      deleteTodo({
+        id,
+        token,
+      }).then((responseData) => {
+        // получили данные и рендерим их в приложении
+        tasks = responseData.todos;
+        renderApp();
+      });
+    });
+  }
+
+  buttonElement.addEventListener("click", () => {
+    if (textInputElement.value === "") {
+      return;
+    }
+
+    buttonElement.disabled = true;
+    buttonElement.textContent = "Задача добавляеятся...";
+
+    addTodo({
+      text: textInputElement.value,
+      token,
+    })
+      .then(() => {
+        // TODO: кинуть исключение
+        textInputElement.value = "";
+      })
+      .then(() => {
+        return fetchTodosAndRender();
+      })
+      .then(() => {
+        buttonElement.disabled = false;
+        buttonElement.textContent = "Добавить";
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("Кажется у вас проблемы с интернетом, попробуйте позже");
+        buttonElement.disabled = false;
+        buttonElement.textContent = "Добавить";
+      });
+  });
+};
+
+renderApp();
